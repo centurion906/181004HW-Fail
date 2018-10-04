4fdb135851274cb38cd29fdfdb35ac77217c1577
centurion906 <32056070+centurion906@users.noreply.github.com>
2018-10-04 11:59:57 +0800

    Add files via upload

---
+++ Decrypt.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Decrypt extends JFrame {
+    Container cp;
+    private JTextArea jtaR = new JTextArea();
+    private JTextArea jtaL = new JTextArea();
+    private JScrollPane jspR = new JScrollPane(jtaR);
+    private JScrollPane jspL = new JScrollPane(jtaL);
+    public Decrypt(){
+        ex();
+    }private void ex(){
+        this.setBounds(100,100,560,400);
+        cp.add()
+    }
+}
